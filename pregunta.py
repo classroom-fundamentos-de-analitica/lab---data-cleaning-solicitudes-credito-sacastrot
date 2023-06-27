81d6d463d434472c684e4ea66eb437d5af0cd05b
sacastrot <sctunal@gmail.com>
2023-06-26 19:06:36 -0500

    Solve attempt 2

--- pregunta.py
+++ pregunta.py
@@ -12,16 +12,19 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
-    df=df.drop(['Unnamed: 0'], axis=1)
-    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
-    df=df.replace(to_replace="(_)|(-)",value=" ",regex=True)    
-    df=df.replace(to_replace="[,$]|(\.00$)",value="",regex=True)
-    df.monto_del_credito = df.monto_del_credito.astype("int")
-    df.comuna_ciudadano = df.comuna_ciudadano.astype("float")
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,infer_datetime_format=True,errors='ignore',dayfirst=True)
-    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
-    df.drop_duplicates(inplace = True)
+
+    df = df.drop('Unnamed: 0', axis = 1)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)  #formato de fecha estandarizado
+
+    df['monto_del_credito'] = df['monto_del_credito'].map(lambda x: str(x).strip('$').replace(',','')) #estandarizando el formato de dinero
+    df.monto_del_credito = df.monto_del_credito.astype(float)   #No deja convertir a int, entonces primero convierto a float
+    df.monto_del_credito = df.monto_del_credito.astype(int)   #Luego a int
+
+    #las columnas de strings pasandolas a minuscula y cambiando - y _ por espacios en blanco
+    df[['sexo','tipo_de_emprendimiento','idea_negocio','barrio','línea_credito']] = df[['sexo','tipo_de_emprendimiento','idea_negocio','barrio','línea_credito']].applymap(lambda s: s.lower().replace('-',' ').replace('_',' ') if type(s) == str else s)
+
+    #dropeo de nulos y duplicados
+    df.dropna(axis=0, inplace = True)
+    df.drop_duplicates(inplace=True)
 
     return df
